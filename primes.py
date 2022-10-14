f79166d01eaeee0b63145f8f89c3fd2a377d1afd
Shih-Hung Lin <shih-hung.lin@kcl.ac.uk>
2022-10-14 11:26:02 +0100

    prime list solution 6

--- primes.py
+++ primes.py
@@ -7,7 +7,7 @@ def primes(number_of_primes):
     if number_of_primes <= 0:
         raise ValueError('Number in argument must be positive')
     for num in range(2, 1000):
-        div = 0;
+        div = 0
         for i in range(2,num):
             if num % i == 0:
                 div += 1
